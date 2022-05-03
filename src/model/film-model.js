7be7069ecc50aa39042719f83f0ca9798fd190c0
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2022-05-03 14:59:26 +0300

    Complete task

--- src/model/film-model.js
+++ src/model/film-model.js
@@ -2,9 +2,14 @@ import { generateFilm } from '../data/film.js';
 import { generateComment } from '../data/comment.js';
 
 export default class FilmsModel {
-  comments = Array.from({length: 101}, generateComment);
-  films = Array.from({length: 20}, generateFilm);
+  #comments = Array.from({length: 101}, generateComment);
+  #films = Array.from({length: 20}, generateFilm);
 
-  getComments = () => this.comments;
-  getFilms = () => this.films;
+  get comments() {
+    return this.#comments;
+  }
+
+  get films() {
+    return this.#films;
+  }
 }
