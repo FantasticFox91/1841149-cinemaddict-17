f942af2794a89242816606ecc44baa8566d1fe8d
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2022-06-11 13:09:00 +0300

    feat: implement human readable date format in comments

--- src/utils/film.js
+++ src/utils/film.js
@@ -1,12 +1,7 @@
 import dayjs from 'dayjs';
 import RelativeTime from 'dayjs/plugin/relativeTime';
-dayjs.extend(RelativeTime);
 
-// const onj = {
-//   'year': [],
-//   'month': [],
-//   'da'
-// }
+dayjs.extend(RelativeTime);
 
 const humanizeYear = (date) => dayjs(date).get('year');
 
@@ -15,30 +10,5 @@ const humanizeDate = (date) => dayjs(date).format('DD MMMM YYYY');
 const humanizeDateAndTime = (date) => dayjs(date).format('YYYY/MM/DD HH:mm');
 
 const humanizeCommentDate = (date) => dayjs(date).fromNow();
-//   const secondDifference = dayjs(Date.now()).diff(dayjs(date), 'second');
-//   const minuteDifference = dayjs(Date.now()).diff(dayjs(date), 'minute');
-//   const hourDifference = dayjs(Date.now()).diff(dayjs(date), 'hour');
-//   const dayDifference = dayjs(Date.now()).diff(dayjs(date), 'day');
-//   const monthDiferenece = dayjs(Date.now()).diff(dayjs(date), 'month');
-//   const yearDiferenece = dayjs(Date.now()).diff(dayjs(date), 'year');
-
-//   console.log(dayjs(date).fromNow());
-
-//   if(yearDiferenece) {
-//     return `${yearDiferenece} ${yearDiferenece > 1 ? 'years' : 'year'} ago`;
-//   } else if (monthDiferenece) {
-//     return `${monthDiferenece} ${monthDiferenece > 1 ? 'years' : 'year'} ago`;
-//   } else if (dayDifference) {
-//     return `${dayDifference} ${dayDifference > 1 ? 'days' : 'day'} ago`;
-//   } else if (hourDifference) {
-//     return `${hourDifference} ${hourDifference > 1 ? 'hours' : 'hour'} ago`;
-//   } else if (minuteDifference) {
-//     return `${minuteDifference} ${minuteDifference > 1 ? 'minutes' : 'minute'} ago`;
-//   } else if (secondDifference) {
-//     return 'Few seconds ago';
-//   } else {
-//     return 'Now';
-//   }
-// };
 
 export { humanizeYear, humanizeDateAndTime, humanizeDate, humanizeCommentDate };
