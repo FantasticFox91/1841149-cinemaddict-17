5e57c2a230b40d437380c2433af8bf0d2b63c5c4
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2022-06-13 12:51:43 +0300

    feat: implement most commented block functionality

--- src/view/film-comments-view.js
+++ src/view/film-comments-view.js
@@ -135,7 +135,7 @@ export default class FilmCommentsView extends AbstractStatefulView {
 
       this.#changeComments(
         UserAction.ADD_COMMENT,
-        UpdateType.MINOR,
+        UpdateType.MAJOR,
         updatedFilm,
         [newComment, newCommentContainer]
       );
